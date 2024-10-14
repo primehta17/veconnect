6bc33df9a1a1d59a372f5b582c9bde703c387468
Priyanka Mehta <priyankamehta@Priyankas-MacBook-Air.local>
2024-10-14 12:07:58 +0530

    backend commit

--- server/server.js
+++
@@ -1,7 +0,0 @@
-let http = require("http")
-let server =http.createServer((req,res)=>{
-    res.write("Hello form node")
-    res.end()
-})
-server.listen(8081)
-console.log("hello from console node port 8080")
